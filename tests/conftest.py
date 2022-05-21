70dbae3b8956ccc018b42126402f86e24fd48195
Tim Johansson <tim.den.vilde@gmail.com>
2022-05-21 11:57:57 +0200

    Refactor tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,9 @@
 import importlib
+import inspect
+from typing import Any, get_args, get_origin, Union
+from unittest.mock import Mock
+
+import pytest
 
 import eventing
 
@@ -10,3 +15,77 @@ import eventing
 # the intended use of the package
 def pytest_runtest_setup(item):
     importlib.reload(eventing)
+
+
+def create_mock_for_parameter(p: inspect.Parameter) -> Any:
+    if p.default is not p.empty:
+        # we have a default, use it
+        return p.default
+
+    if p.kind is p.VAR_POSITIONAL:
+        return ()
+
+    if p.kind is p.VAR_KEYWORD:
+        return {}
+
+    if p.annotation is p.empty:
+        # we have no annotation to go off of, just give it a plain mock
+        return Mock()
+
+    origin = get_origin(p.annotation)
+    if origin is None and p.annotation:
+        # Annotation is probably just a regular type or class
+        return Mock(p.annotation)
+    elif origin is Union:
+        return Mock(get_args(p.annotation)[0])
+    else:  # last resort
+        return Mock()
+
+
+@pytest.fixture
+def auto_bind_func_with_mocks_from_signature():
+    def wrapper(func, /):
+        def _auto_bind_func_with_mocks_from_signature(**arg_overrides):
+            """Tries to mock a call to the function.
+
+            The function signature will be inspected and all arguments not
+            provided by `arg_overrides` will be substituted with a Mock that
+            is the best attempt to match the type. Default values are left as
+            default. If a type is a Union, the first type in it will be
+            provided.
+
+            Args:
+                **arg_overrides:
+                    argument overrides provided in a keyword fashion. These
+                    will be correctly substituted in the function, even if
+                    positional only.
+
+            Returns:
+                Whatever `func` returns.
+            """
+            sig = inspect.signature(func)
+            args, kwargs = [], {}
+            for p in sig.parameters.values():
+                try:
+                    # we have and override, use it
+                    arg = arg_overrides[p.name]
+                except KeyError:
+                    # try to create a mock that would pass as the argument
+                    arg = create_mock_for_parameter(p)
+
+                if p.kind in [p.POSITIONAL_ONLY, p.POSITIONAL_OR_KEYWORD]:
+                    args.append(arg)
+                elif p.kind == p.VAR_POSITIONAL:
+                    args.extend(arg)
+                elif p.kind == p.KEYWORD_ONLY:
+                    kwargs[p.name] = arg
+                elif p.kind == p.VAR_KEYWORD:
+                    kwargs.update(arg)
+                else:
+                    raise ValueError("Unknown parameter kind encountered.")
+
+            return func(*args, **kwargs)
+
+        return _auto_bind_func_with_mocks_from_signature
+
+    return wrapper
--- tests/test_event_names.py
+++ tests/test_event_names.py
@@ -1,44 +1,28 @@
-from collections import namedtuple
-import functools
-
 import pytest
 
 import eventing
 
 
-def a_listener():
-    pass
-
-
-FuncBindSign = namedtuple("FuncBindSign", ["func_name", "params_to_bind"])
-
-functions_using_event_name_with_args_signatures = [
-    FuncBindSign("listener_count", {}),
-    FuncBindSign("listeners", {}),
-    FuncBindSign("add_listener", {"listener": a_listener}),
-    FuncBindSign("remove_listener", {"listener": a_listener}),
-    FuncBindSign("emit", {}),
-]
-
-
-def get_function_bound_for_call_with_event_name_only(
-    ee: eventing.EventEmitter, func_bind_sign: FuncBindSign
-):
-    func = getattr(ee, func_bind_sign.func_name)
-    return functools.partial(func, **func_bind_sign.params_to_bind)
-
-
-@pytest.fixture(params=functions_using_event_name_with_args_signatures)
-def func_wrapped_for_event_name(request):
+@pytest.fixture(
+    params=[
+        "listener_count",
+        "listeners",
+        "add_listener",
+        "remove_listener",
+        "emit",
+    ]
+)
+def mocked_event_name_func(request, auto_bind_func_with_mocks_from_signature):
     ee = eventing.get_emitter()
-    return get_function_bound_for_call_with_event_name_only(ee, request.param)
+    func = getattr(ee, request.param)
+    return auto_bind_func_with_mocks_from_signature(func)
 
 
-def test_event_names_may_only_be_strings(func_wrapped_for_event_name):
+def test_event_names_may_only_be_strings(mocked_event_name_func):
     with pytest.raises(TypeError):
-        func_wrapped_for_event_name(False)
+        mocked_event_name_func(event_name=False)
 
 
-def test_event_name_may_not_be_empty(func_wrapped_for_event_name):
+def test_event_name_may_not_be_empty(mocked_event_name_func):
     with pytest.raises(ValueError, match="must not be empty"):
-        func_wrapped_for_event_name("")
+        mocked_event_name_func(event_name="")

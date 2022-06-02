6c2579f4224ffac334d8a92ecbd50fd915dccbd9
Tim Johansson <tim.den.vilde@gmail.com>
2022-06-02 22:37:21 +0200

    Add skipped tests for wishful behavior
    That the @ee.on(...) decorator does the "right" thing when it comes to methods on classes.
     - @staticmethod - should work out-of-the-box due to how the decorator works
     - @classmethod - preferably a _bound_ classmethod listener is added
     - instance methods - preferably the _bound_ instance methods are only added when the class is instatiated
    
    I don't know if this behavior would be a good idea as it'd would break the expectations of how decorators normally work, and from experiments be rather complex. Maybe a keyword argument to opt-in to the behavior?

--- tests/test_on.py
+++ tests/test_on.py
@@ -14,3 +14,67 @@ def test_listener_with_on_decorator_is_added_to_listeners(ee):
         pass
 
     assert ee.listeners("foo") == [listener]
+
+
+METHODS_ON_CLASS_SKIP_REASON = (
+    "Wishful functionality; might not be a good idea"
+)
+
+
+@pytest.mark.skip(METHODS_ON_CLASS_SKIP_REASON)
+def test_instancemethod_decorated_with_on_isnt_added_until_instantiated(ee):
+    class Foo:
+        @ee.on("foo")
+        def listener(self):
+            pass
+
+    assert ee.listeners("foo") == []
+    foo_instance = Foo()
+    assert ee.listeners("foo") == [foo_instance.listener]
+
+
+@pytest.mark.skip(METHODS_ON_CLASS_SKIP_REASON)
+def test_instancemethod_decorated_with_on_is_correctly_bound_when_called(ee):
+    called_arg = None
+
+    class Foo:
+        @ee.on("foo")
+        def listener(self):
+            nonlocal called_arg
+            called_arg = self
+
+    foo_instance = Foo()
+    ee.emit("foo")
+    assert called_arg == foo_instance
+
+
+@pytest.mark.skip(METHODS_ON_CLASS_SKIP_REASON)
+def test_classmethod_decorated_with_on_is_correctly_bound_when_called(ee):
+    called_arg = None
+
+    class Foo:
+        @classmethod
+        @ee.on("foo")
+        def listener(cls):
+            nonlocal called_arg
+            called_arg = cls
+
+    assert ee.listeners("foo") == [Foo.listener]
+    ee.emit("foo")
+    assert called_arg == Foo
+
+
+@pytest.mark.skip(METHODS_ON_CLASS_SKIP_REASON)
+def test_staticmethod_decorated_with_on_is_correctly_called(ee):
+    called = False
+
+    class Foo:
+        @staticmethod
+        @ee.on("foo")
+        def listener():
+            nonlocal called
+            called = True
+
+    assert ee.listeners("foo") == [Foo.listener]
+    ee.emit("foo")
+    assert called
